f2d92fed34d2212f0731c242b26e7d43821d5d48
Martin <62774491+martinogarrizzo5@users.noreply.github.com>
2023-03-16 09:43:47 +0100

    add validation library with example

--- controllers/factory.ts
+++ controllers/factory.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import prisma from "../prisma/db_connection";
+import { z } from "zod";
 
 export const getFactories: RequestHandler = async (req, res) => {
   const data = await prisma.factory.findMany({ orderBy: { name: "asc" } });
@@ -10,21 +11,35 @@ export const addFactory: RequestHandler = async (req, res) => {
   const factoryAddress = req.body.address;
   const factoryName = req.body.name;
 
-  const factory = await prisma.factory.create({
+  await prisma.factory.create({
     data: { address: factoryAddress, name: factoryName },
   });
-  return res.json();
+
+  return res
+    .status(201)
+    .json({ message: "Stabilimento aggiunto con successo" });
 };
 
-export const updateFactory: RequestHandler = async (req, res) => {
-  const factoryAddress = req.body.address;
-  const factoryName = req.body.name;
+const updateSchema = z.object({
+  address: z.string().min(1),
+  name: z.string().min(1),
+});
 
-  const factory = await prisma.factory.update({
-    where: { id: +req.params.id },
-    data: { address: factoryAddress, name: factoryName },
+export const updateFactory: RequestHandler = async (req, res) => {
+  const id = req.params.id;
+  const result = updateSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({ message: "Valori inseriti invalidi" });
+  }
+
+  const { address, name } = result.data;
+
+  await prisma.factory.update({
+    where: { id: +id },
+    data: { address: address, name: name },
   });
-  return res.json();
+
+  return res.json({ message: "Stabilimento aggiornato con successo" });
 };
 
 export const deleteFactory: RequestHandler = async (req, res) => {
@@ -33,5 +48,6 @@ export const deleteFactory: RequestHandler = async (req, res) => {
   const factory = await prisma.factory.delete({
     where: { id: factoryId },
   });
+
   return res.json();
 };
