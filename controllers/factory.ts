e2c101624f1d2bd65d61d7b030336d15b34014f3
RPette <118899569+RPette@users.noreply.github.com>
2023-03-16 09:03:27 +0100

    Add Factory Controllers

---
+++ controllers/factory.ts
@@ -0,0 +1,37 @@
+import { RequestHandler } from "express";
+import prisma from "../prisma/db_connection";
+
+export const getFactories: RequestHandler = async (req, res) => {
+  const data = await prisma.factory.findMany({ orderBy: { name: "asc" } });
+  return res.json(data);
+};
+
+export const addFactory: RequestHandler = async (req, res) => {
+  const factoryAddress = req.body.address;
+  const factoryName = req.body.name;
+
+  const factory = await prisma.factory.create({
+    data: { address: factoryAddress, name: factoryName },
+  });
+  return res.json();
+};
+
+export const updateFactory: RequestHandler = async (req, res) => {
+  const factoryAddress = req.body.address;
+  const factoryName = req.body.name;
+
+  const factory = await prisma.factory.update({
+    where: { id: +req.params.id },
+    data: { address: factoryAddress, name: factoryName },
+  });
+  return res.json();
+};
+
+export const deleteFactory: RequestHandler = async (req, res) => {
+  const factoryId = +req.params.id;
+
+  const factory = await prisma.factory.delete({
+    where: { id: factoryId },
+  });
+  return res.json();
+};
