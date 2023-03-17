38171a5c6cb973e4925ccccaca35847ff21df6e7
Martin <62774491+martinogarrizzo5@users.noreply.github.com>
2023-03-17 12:14:14 +0100

    add report functionality

--- controllers/report.ts
+++ controllers/report.ts
@@ -1,19 +1,28 @@
 import { RequestHandler } from "express";
-import { date } from "zod";
 import prisma from "../prisma/db_connection";
 
-//deve ritornare su una webpage la tabella con tutti i dati
+/* 
+  return a list with all factories and the date of the last
+  injury on each of them
+*/
 export const getReport: RequestHandler = async (req, res) => {
-  const lastInjuryDate = await prisma.injury.groupBy({
-    by: ["factoryId"],
-    _max: { date: true },
+  const factoriesWithLastInjury = await prisma.factory.findMany({
+    include: {
+      injuries: {
+        orderBy: { date: "asc" },
+        take: 1,
+      },
+    },
   });
 
-  const factory = await prisma.factory.findMany();
-
-  const result = lastInjuryDate.map((element) => ({
-    factory: factory.find((factory) => factory.id === element.factoryId),
-    lastInjuryDate: element._max.date,
+  const formattedData = factoriesWithLastInjury.map((factory) => ({
+    id: factory.id,
+    name: factory.name,
+    address: factory.address,
+    lastInjuryDate: factory.injuries.length ? factory.injuries[0].date : null,
   }));
-  res.json(result);
+
+  res.json(formattedData);
 };
+
+export const getFactoryHistory: RequestHandler = async (req, res) => {};
--- routes/report.ts
+++ routes/report.ts
@@ -4,5 +4,6 @@ import * as reportController from "../controllers/report";
 const router = Router();
 
 router.get("/report", reportController.getReport);
+router.get("/report/:factoryId", reportController.getFactoryHistory);
 
 export default router;
