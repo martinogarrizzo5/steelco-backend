9ef55b5123a3334bbf8897735f6c56abba1f8cba
RPette <118899569+RPette@users.noreply.github.com>
2023-03-18 21:47:53 +0100

    Display factory list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <div className="App">app</div>;
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const GetFactories =  () => {
+  const [factories, setfactories] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/factory").then((response) => {
+    setfactories(response.data)
+  });
+  }, []);  
+
+  return (
+    <div>
+      <ul>
+        {
+          factories.map(factory => <li key={factory.id.toLocaleString()}>{factory.address.toLocaleString()}{" - "}{factory.name.toLocaleString()}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+  return <GetFactories />
 }
 
 export default App;
+
