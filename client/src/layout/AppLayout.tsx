c69afde22eb5fc0dd5ea98c4b1ce6df4cb3a2a00
Martin Meneghetti <62774491+martinogarrizzo5@users.noreply.github.com>
2023-03-20 13:05:15 +0100

    add click on logo

--- client/src/layout/AppLayout.tsx
+++ client/src/layout/AppLayout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import logoImg from "../assets/logo.png";
 
 function AppLayout() {
+  const navigate = useNavigate();
+
   return (
     <>
       <img
         src={logoImg}
         alt="logo"
-        className="h-10 sm:h-14 mx-auto mt-3 mb-6 sm:mb-8"
+        className="h-10 sm:h-14 mx-auto mt-3 mb-6 sm:mb-8 cursor-pointer"
+        onClick={() => navigate("/app/factory")}
       />
       <Outlet />
     </>
