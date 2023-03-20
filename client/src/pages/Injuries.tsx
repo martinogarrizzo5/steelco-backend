f489b69dbb1cb7cbe4cfd60fbe492783ffd7fd13
Martin Meneghetti <62774491+martinogarrizzo5@users.noreply.github.com>
2023-03-20 13:05:25 +0100

    refactor chart code

--- client/src/pages/Injuries.tsx
+++ client/src/pages/Injuries.tsx
@@ -4,79 +4,78 @@ import { AxisOptions, Chart } from "react-charts";
 import ReactDOM from "react-dom";
 
 type DailyStars = {
-    date: Date,
-    stars: number,
-  }
-  
-  type Series = {
-    label: string,
-    data: DailyStars[]
-  }
-  
-  const data: Series[] = [
-    {
-      label: 'React Charts',
-      data: [
-        {
-          date: new Date(),
-          stars: 202123,
-        }
-        // ...
-      ]
-    },
-    {
-      label: 'React Query',
-      data: [
-        {
-          date: new Date(),
-          stars: 10234230,
-        }
-        // ...
-      ]
-    },
-    {
-      label: 'React Charts',
-      data: [
-        {
-          date: new Date(),
-          stars: 202123,
-        }
-        // ...
-      ]
-    }
-  ]
-  
-function InjuriesScreen (){
-    
-    const primaryAxis = React.useMemo(
-        (): AxisOptions<DailyStars> => ({
-          getValue: datum => datum.date,
-        }),
-        []
-      )
-    
-      const secondaryAxes = React.useMemo(
-        (): AxisOptions<DailyStars>[] => [
-          {
-            getValue: datum => datum.stars,
-          },
-        ],
-        []
-      )
-    
-      return (
-        <><div>
-              <Chart
-                  options={{
-                      data,
-                      primaryAxis,
-                      secondaryAxes,
-                  }} />
-          </div>
-          <div>
-                  <p>porcamandoona</p>
-              </div></>
-      )
+  date: Date;
+  stars: number;
+};
+
+type Series = {
+  label: string;
+  data: DailyStars[];
+};
+
+const data: Series[] = [
+  {
+    label: "React Charts",
+    data: [
+      {
+        date: new Date(),
+        stars: 202123,
+      },
+      // ...
+    ],
+  },
+  {
+    label: "React Query",
+    data: [
+      {
+        date: new Date(),
+        stars: 10234230,
+      },
+      // ...
+    ],
+  },
+  {
+    label: "React Charts",
+    data: [
+      {
+        date: new Date(),
+        stars: 202123,
+      },
+      // ...
+    ],
+  },
+];
+
+function InjuriesScreen() {
+  const primaryAxis = React.useMemo(
+    (): AxisOptions<DailyStars> => ({
+      getValue: datum => datum.date,
+    }),
+    []
+  );
+
+  const secondaryAxes = React.useMemo(
+    (): AxisOptions<DailyStars>[] => [
+      {
+        getValue: datum => datum.stars,
+      },
+    ],
+    []
+  );
+
+  return (
+    <main className="max-w-4xl mx-auto mb-4 px-6">
+      <div className="w-full h-40">
+        <Chart
+          options={{
+            data,
+            primaryAxis,
+            secondaryAxes,
+          }}
+        />
+      </div>
+    </main>
+  );
 }
 
-export default InjuriesScreen
+export default InjuriesScreen;
